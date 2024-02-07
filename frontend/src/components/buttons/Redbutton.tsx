e9f9ff020c20d6a42d5a436fcf2f7e455a8515aa
othman <othmandn1@gmail.com>
2024-02-07 16:52:59 +0100

    finished first part of home page

---
+++ frontend/src/components/buttons/Redbutton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type RedButtonProps = {
+  text: string;
+  padding: string;
+  isBold: boolean;
+  size: string;
+  link: string;
+};
+
+const RedButton = ({ text, padding, isBold, size, link }: RedButtonProps) => {
+  const buttonClasses = `${padding} ${
+    isBold ? "font-bold" : "font-normal"
+  } text-white bg-red-500 rounded-xl text-${size} w-[9dvw]`;
+
+  return <button className={buttonClasses}>{text}</button>;
+};
+
+export default RedButton;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,31 @@
+import Image from "next/image";
+import Redbutton from "@/components/buttons/Redbutton";
+import preview from "@/assets/home_preview.png";
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1 className="text-xl text-blue-500"> HOME PAGE</h1>
-      </div>
-    </main>
+    <section className="custom-bg w-full">
+      <section className="flex justify-between w-[70%] mx-auto">
+        <div className="text-white">
+          <h1 className="text-4xl w-[60%]">
+            Libérez votre créativité avec{" "}
+            <span className="text-[#E83B4E]">MailCraft</span>
+          </h1>
+          <p className="w-[30%] leading-[30px] my-5">
+            Tout ce dont vous avez besoin pour vos modèles d'e-mails
+            personnalisés en une seule plateforme, prêt à l'envoi en quelques
+            minutes. Glissez, déposez , envoyez !
+          </p>
+
+          <Redbutton
+            text="Je m'inscris !"
+            padding={"p-3"}
+            isBold={false}
+            size={"lg"}
+            link={"/"}
+          />
+        </div>
+        <Image src={preview} alt="app preview" width={300} height={200} />
+      </section>
+    </section>
   );
 }
