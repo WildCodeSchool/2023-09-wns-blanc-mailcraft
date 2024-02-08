cfda65d97ae02d7e1ca068447920f5f55134d237
DRISS BOUMEDIENE <[driss308@hotmail.fr](mailto:driss308@hotmail.fr)>
2024-02-08 11:51:48 +0100

    added pages to navbar links

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
+
 export default function NavBar() {
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MailCraft</span>
+                    <img src="images/logo.png" className="h-[8dvh] w-[11dvw]" alt="Flowbite Logo" />
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     <button type="button" className="text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-200 font-medium rounded-lg text-md px-4 py-2 text-center">Mon compte</button>
@@ -18,13 +20,13 @@ export default function NavBar() {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <a href="#" className="block py-2 px-3 md:p-0 text-white bg-red-500 rounded md:bg-transparent md:text-red-500" aria-current="page">Accueil</a>
+                            <Link href="/" className="block py-2 px-3 md:p-0 text-white bg-red-500 rounded md:bg-transparent md:text-red-500" aria-current="page">Accueil</Link>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Ressouces</a>
+                            <Link href="/ressources" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Ressouces</Link>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Prix</a>
+                            <Link href="/prix" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Prix</Link>
                         </li>
                         <li>
                             <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Déconnexion</a>
