e31b8c2912078c6f4ca1f7d618901906c3b3d061
DRISS BOUMEDIENE <[driss308@hotmail.fr](mailto:driss308@hotmail.fr)>
2024-02-08 11:31:46 +0100

    added navbar component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import HomeCard from "@/components/Cards/HomeCard";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <main>
       <div>
         <h1 className="text-xl text-blue-500">HOME PAGE</h1>
+        <>
+          <NavBar />
+        </>
       </div>
     </main>
   );
