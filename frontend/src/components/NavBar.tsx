87c9c66165a4e58e5ecf721500603c067e26b285
DRISS BOUMEDIENE <[driss308@hotmail.fr](mailto:driss308@hotmail.fr)>
2024-02-08 19:30:42 +0100

    changed font

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function NavBar() {
     return (
         <nav>
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-8 mb-10">
                 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="images/logo.png" className="h-[8dvh] w-[11dvw]" alt="Flowbite Logo" />
+                    <img src="images/logo.png" className="h-[5dvh] w-[26dvw] md:h-[8dvh] md:w-[11dvw]" alt="Flowbite Logo" />
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" className="text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-200 font-medium rounded-lg text-md px-4 py-2 text-center">Mon compte</button>
+                    <button type="button" className="text-white text-md bg-[#E83B4E] hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-200 font-medium rounded-lg px-3 md:px-4 py-2 text-center">S'inscrire</button>
                     <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                         <span className="sr-only">Ouvre le menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -18,18 +18,18 @@ export default function NavBar() {
                     </button>
                 </div>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-20 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                         <li>
-                            <Link href="/" className="block py-2 px-3 md:p-0 text-white bg-red-500 rounded md:bg-transparent md:text-red-500" aria-current="page">Accueil</Link>
+                            <Link href="/" className="block py-2 px-3 md:p-0 text-xl text-white bg-red-500 rounded md:bg-transparent md:text-red-500" aria-current="page">Accueil</Link>
                         </li>
                         <li>
-                            <Link href="/ressources" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Ressouces</Link>
+                            <Link href="/ressources" className="block py-2 px-3 md:p-0 text-xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Ressouces</Link>
                         </li>
                         <li>
-                            <Link href="/prix" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Prix</Link>
+                            <Link href="/prix" className="block py-2 px-3 md:p-0 text-xl text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Prix</Link>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Déconnexion</a>
+                            <a href="#" className="block py-2 px-3 md:p-0 text-lg text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500">Déconnexion</a>
                         </li>
                     </ul>
                 </div>
