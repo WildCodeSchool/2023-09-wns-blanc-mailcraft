d5e01e9f70b927a141e7d887a59037e6eb9e1ee5
othman <othmandn1@gmail.com>
2024-03-16 14:03:06 +0100

    dragNdrop initialized

---
+++ frontend/src/components/dragNdrop/DesignCard.tsx
@@ -0,0 +1,151 @@
+import boldIcon from "@/assets/template-page/boldIcon.png";
+import italicIcon from "@/assets/template-page/italicIcon.png";
+import underlineIcon from "@/assets/template-page/underlineIcon.png";
+import alignLeft from "@/assets/template-page/alignLeft.png";
+import alignCenter from "@/assets/template-page/alignCenter.png";
+import alignRight from "@/assets/template-page/alignRight.png";
+import { useState } from "react";
+
+interface StyleIconProps {
+  src: string;
+  alt: string;
+}
+
+interface JustificationIconProps {
+  src: string;
+  alt: string;
+}
+
+export default function DesignCard() {
+  const [selectedStyleIconIndex, setSelectedStyleIconIndex] = useState<
+    number | null
+  >(null);
+  const [selectedJustificationIconIndex, setSelectedJustificationIconIndex] =
+    useState<number | null>(null);
+
+  const handleStyleIconClick = (index: number) => {
+    setSelectedStyleIconIndex(index === selectedStyleIconIndex ? null : index);
+  };
+
+  const handleJustificationIconClick = (index: number) => {
+    setSelectedJustificationIconIndex(
+      index === selectedJustificationIconIndex ? null : index
+    );
+  };
+
+  const styleIcons: StyleIconProps[] = [
+    { src: boldIcon.src, alt: "Bold Icon" },
+    { src: italicIcon.src, alt: "Italic Icon" },
+    { src: underlineIcon.src, alt: "Underline Icon" },
+  ];
+
+  const justificationIcons: JustificationIconProps[] = [
+    { src: alignLeft.src, alt: "Align left Icon" },
+    { src: alignCenter.src, alt: "Align center Icon" },
+    { src: alignRight.src, alt: "Align right Icon" },
+  ];
+
+  return (
+    <div className="w-72 max-w-sm p-4 bg-red-400 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
+      <form className="space-y-4" action="#">
+        <h5 className="text-xl font-medium text-gray-900 text-center">
+          Design
+        </h5>
+        <div>
+          <label
+            htmlFor="police"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Police
+          </label>
+          <input
+            type="text"
+            name="police"
+            id="police"
+            placeholder="Arial"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="size"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Taille
+          </label>
+          <input
+            type="text"
+            name="size"
+            id="size"
+            placeholder="12"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="color"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Couleur
+          </label>
+          <input
+            type="text"
+            name="color"
+            id="color"
+            placeholder="Black"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="style"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Style
+          </label>
+          <div className="flex bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-sm">
+            {styleIcons.map((icon, index) => (
+              <div
+                key={index}
+                className={`styleIcon flex-1 ${
+                  index === styleIcons.length - 1 ? "" : "border-r"
+                } border-gray-300 flex justify-center items-center cursor-pointer ${
+                  selectedStyleIconIndex === index ? "bg-gray-300" : ""
+                }`}
+                onClick={() => handleStyleIconClick(index)}
+              >
+                <img src={icon.src} className="h-6" alt={icon.alt} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <label
+            htmlFor="justification"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Justification
+          </label>
+          <div className="flex bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-sm">
+            {justificationIcons.map((icon, index) => (
+              <div
+                key={index}
+                className={`justificationIcon ${
+                  index === justificationIcons.length - 1 ? "" : "border-r"
+                } flex-1 border-gray-300 flex justify-center items-center cursor-pointer ${
+                  selectedJustificationIconIndex === index ? "bg-gray-300" : ""
+                }`}
+                onClick={() => handleJustificationIconClick(index)}
+              >
+                <img src={icon.src} className="h-6" alt={icon.alt} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
--- frontend/src/pages/template.tsx
+++ frontend/src/pages/template.tsx
@@ -1,78 +1,209 @@
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-import Surface from "@/components/drag&drop/Surface";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { useState, useRef, FunctionComponent } from "react";
+import Image from "next/image";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
+import DesignCard from "@/components/dragNdrop/DesignCard";
+import imageIconSrc from "../assets/template-page/icon-image.png";
+import texteIconSrc from "../assets/template-page/icon-texte.png";
+import logoIconSrc from "../assets/template-page/lien-de-partage.png";
 
-// Define the color keys type
-type ColorKey = "Rouge" | "Bleu" | "Vert";
-
-// Define the initial item type
-type Item = {
+interface IListElement {
   id: string;
-  content: ColorKey;
+  title: string;
+  picture: any;
+}
+
+interface IZone {
+  id: string;
+  moduleType: string;
+}
+
+interface ModuleProps {
+  title: string;
+  picture: any;
+}
+
+const Module: FunctionComponent<ModuleProps> = ({ title, picture }) => {
+  return (
+    <div>
+      <Image src={picture} alt={title} width={40} height={40} />
+      <p>{title}</p>
+    </div>
+  );
 };
 
-const initialItems: Item[] = [
-  { id: "item-1", content: "Rouge" },
-  { id: "item-2", content: "Bleu" },
-  { id: "item-3", content: "Vert" },
-];
+const TemplatePage: FunctionComponent = () => {
+  const [zones, setZones] = useState<IZone[]>([]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-// Define the color mapping
-const colorMapping: Record<ColorKey, string> = {
-  Rouge: "border-red-900",
-  Bleu: "border-blue-900",
-  Vert: "border-green-900",
-};
+  const listElements: IListElement[] = [
+    {
+      id: "1",
+      title: "Texte",
+      picture: texteIconSrc,
+    },
+    {
+      id: "2",
+      title: "Image",
+      picture: imageIconSrc,
+    },
+    {
+      id: "3",
+      title: "Logo",
+      picture: logoIconSrc,
+    },
+  ];
 
-export default function templatePage() {
-  const [items, setItems] = useState<Item[]>(initialItems);
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) return;
 
-  function handleOnDragEnd(result: any) {
-    if (!result.destination) return;
+    const newZones = zones.map((zone) => {
+      if (zone.id === destination.droppableId) {
+        const droppedElement = listElements.find((el) => el.id === draggableId);
+        return {
+          ...zone,
+          moduleType: droppedElement ? droppedElement.title.toLowerCase() : "",
+        };
+      }
+      return zone;
+    });
 
-    const updatedItems = Array.from(items);
-    const [reorderedItem] = updatedItems.splice(result.source.index, 1);
-    updatedItems.splice(result.destination.index, 0, reorderedItem);
+    setZones(newZones);
+  };
 
-    setItems(updatedItems);
-  }
+  const createZones = (number: number) => {
+    const newZones = Array.from({ length: number }).map((_, index) => ({
+      id: `zone-${index}`,
+      moduleType: "",
+    }));
+    setZones(newZones);
+  };
+
+  const removeZone = (zoneId: string) => {
+    const newZones = zones.filter((zone) => zone.id !== zoneId);
+    setZones(newZones);
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.files);
+  };
 
   return (
-    <DragDropContext onDragEnd={handleOnDragEnd}>
+    <>
+      <section className="flex gap-2">
+        {[1, 2, 3].map((number) => (
+          <button
+            key={number}
+            className="border-2 border-gray-950 p-2"
+            onClick={() => createZones(number)}
+          >
+            {number} ZONE{number > 1 ? "S" : ""}
+          </button>
+        ))}
+      </section>
       <section className="w-full flex justify-between">
-        <Droppable droppableId="droppable-1">
-          {(provided) => (
-            <section
-              className="w-30% flex flex-col gap-5 border-solid border-4 border-black p-5 ml-10"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              <h1>Section 1</h1>
-              {items.map((item, index) => {
-                const itemContent: ColorKey = item.content; // Cast to ColorKey type
-                return (
-                  <Draggable key={item.id} draggableId={item.id} index={index}>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="elements" direction="horizontal">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="flex flex-col items-center gap-5 overflow-auto p-4 bg-[#f4f4f4] w-[20%] mt-5"
+                style={{ minHeight: "100px" }}
+              >
+                {listElements.map((el, index) => (
+                  <Draggable key={el.id} draggableId={el.id} index={index}>
                     {(provided) => (
                       <div
-                        className={`border-4 border-solid ${colorMapping[itemContent]}`}
+                        ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
-                        ref={provided.innerRef}
+                        className="p-2 m-2 bg-white border w-[50%]
+                        flex justify-center"
                       >
-                        {item.content}
+                        <Module title={el.title} picture={el.picture} />
                       </div>
                     )}
                   </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </section>
-          )}
-        </Droppable>
-        <Surface />
-        <section className="invisible"></section>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+
+          {/* Zones container */}
+          <section className="flex flex-col justify-center items-center w-[40%] h-[90vh] border-solid border-2 border-gray-300 p-10 bg-[#E83B4E] my-4">
+            <button className="w-1/2 h-[10%] mb-4" onClick={() => setZones([])}>
+              Clear Zones
+            </button>
+            <div className="border-2 border-solid border-gray-950 w-full h-full flex justify-center items-center p-4 bg-[#fff] flex-col gap-5 overflow-auto">
+              {zones.map((zone, index) => (
+                <Droppable key={zone.id} droppableId={zone.id}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={`flex flex-col justify-center items-center w-full min-h-[100px] border-2 ${
+                        snapshot.isDraggingOver
+                          ? "bg-green-100"
+                          : "border-dashed border-gray-500"
+                      } p-4 relative m-2`}
+                    >
+                      {zone.moduleType === "texte" && (
+                        <textarea
+                          className="w-full h-40 p-4 border-2 border-gray-300"
+                          placeholder="YOUR TEXT HERE"
+                        ></textarea>
+                      )}
+                      {zone.moduleType === "image" && (
+                        <button onClick={() => fileInputRef.current.click()}>
+                          <Image
+                            src={imageIconSrc}
+                            alt="app preview"
+                            width={40}
+                            height={40}
+                          />
+                        </button>
+                      )}
+                      {zone.moduleType === "logo" && (
+                        <button onClick={() => fileInputRef.current.click()}>
+                          <Image
+                            src={logoIconSrc}
+                            alt="app preview"
+                            width={40}
+                            height={40}
+                          />
+                        </button>
+                      )}
+                      <button
+                        className="absolute top-0 right-0 p-2"
+                        onClick={() => removeZone(zone.id)}
+                      >
+                        ✕
+                      </button>
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              ))}
+            </div>
+          </section>
+        </DragDropContext>
+        <DesignCard />
       </section>
-    </DragDropContext>
+      <input
+        type="file"
+        hidden
+        ref={fileInputRef}
+        onChange={handleFileChange}
+      />
+    </>
   );
-}
+};
+
+export default TemplatePage;
