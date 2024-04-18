27cd509a86d2a4a6bf30375dd75c70caeb7271ca
Mael <mael.vincent.pro@gmail.com>
2024-04-18 17:08:30 +0200

    Fix next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  compiler: {
+    reactRemoveProperties: true,
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
