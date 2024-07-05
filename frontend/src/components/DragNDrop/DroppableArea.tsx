3487fa899bcaeecb51d29d2ee9cfcb49b1ee3e72
DRISS BOUMEDIENE <[driss308@hotmail.fr](mailto:driss308@hotmail.fr)>
2024-07-05 17:51:03 +0200

    fix responsive droppable area

--- frontend/src/components/DragNDrop/DroppableArea.tsx
+++ frontend/src/components/DragNDrop/DroppableArea.tsx
@@ -16,12 +16,12 @@ const DroppableArea = () => {
             {...provided.droppableProps}
             className="flex flex-col justify-center items-start gap-6 2xl:gap-28 p-2 bg-white h-full border border-gray-300 rounded-lg shadow-xl"
           >
-            <div className="flex flex-col w-full gap-3 ms-2">
-              <h1 className="font-medium text-xl">Structures</h1>
+            <div className="flex flex-col 2xl:items-center w-full gap-3 ms-2">
+              <h1 className="font-medium text-xl 2xl:mb-3">Structures</h1>
               <ZoneCreation />
             </div>
-            <div className="flex flex-col items-start w-full gap-3 ms-2">
-              <h1 className="font-medium text-xl">Modules</h1>
+            <div className="flex flex-col items-start 2xl:items-center w-full gap-3 ms-2">
+              <h1 className="font-medium text-xl 2xl:mb-3">Modules</h1>
               <div className="grid grid-cols-2 gap-5">
                 {listElements.map((el, index) => (
                   <Draggable
