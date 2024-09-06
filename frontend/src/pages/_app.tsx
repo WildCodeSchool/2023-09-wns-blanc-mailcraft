a37e44589ca324cc04fc1c6d204323357c183079
ablah31 <ibrahimekaloko31@gmail.com>
2024-09-06 14:10:49 +0200

    Server resolve

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -20,7 +20,7 @@ Modal.setAppElement("#__next");
 const Layout = dynamic(() => import("@/components/Layout"), { ssr: false });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql", // temporaire à passer en variable d'env
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI || "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
